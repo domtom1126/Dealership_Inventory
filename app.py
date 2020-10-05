7cb6723d65b3af19fafcc6ca268eb34c84baf7bb
domtom1126 <domtom1126@gmail.com>
2020-10-04 21:23:03 -0700

    Dashboard now greets user based on thier login info.

--- app.py
+++ app.py
@@ -1,21 +1,28 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 import sqlite3
 
 app = Flask(__name__)
 conn = sqlite3.connect('user.db')
 
 
-@app.route('/', methods=['post', 'get'])
+@app.route('/', methods=['POST', 'GET'])
 def login():
-    username = request.form.get('username', False)
-    password = request.form.get('password', False)
-    if request.method == 'POST':
-        conn = sqlite3.connect('user.db')
-        cur = conn.cursor()
-        cur.execute('INSERT INTO user (username, password) VALUES (?,?)', (username, password))
-        conn.commit()
-        conn.close()
+    # username = request.form.get('username')
+    # password = request.form.get('password', False)
+    # if request.method == 'POST':
+    #     conn = sqlite3.connect('user.db')
+    #     cur = conn.cursor()
+    #     cur.execute('INSERT INTO user (username, password) VALUES (?,?)', (username, password))
+    #     conn.commit()
+    #     conn.close()
     return render_template('login.html')
+
+@app.route('/dashboard', methods=['POST','GET'])
+def dashboard():
+    if request.method == 'POST':
+        username = request.form['username']
+    return render_template('dashboard.html', username = username )
+
 
 if __name__ == '__main__':
     app.run(debug=True)
