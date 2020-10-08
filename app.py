9c43e21485127cf22cf0d6df41bbc5dcb72d4f53
domtom1126 <domtom1126@gmail.com>
2020-10-08 00:03:18 -0700

    Fully functional form to database, adding a car adds to databse, selling a car deletes from database

--- app.py
+++ app.py
@@ -5,24 +5,61 @@ app = Flask(__name__)
 conn = sqlite3.connect('user.db')
 
 
+
 @app.route('/', methods=['POST', 'GET'])
 def login():
-    # username = request.form.get('username')
-    # password = request.form.get('password', False)
-    # if request.method == 'POST':
-    #     conn = sqlite3.connect('user.db')
-    #     cur = conn.cursor()
-    #     cur.execute('INSERT INTO user (username, password) VALUES (?,?)', (username, password))
-    #     conn.commit()
-    #     conn.close()
     return render_template('login.html')
 
 @app.route('/dashboard', methods=['POST','GET'])
 def dashboard():
     if request.method == 'POST':
         username = request.form['username']
+        password = request.form['password']
+        conn = sqlite3.connect('user.db')
+        cur = conn.cursor()
+        cur.execute('INSERT INTO user (username, password) VALUES (?,?)', (username, password))
+        conn.commit()
+        conn.close()
     return render_template('dashboard.html', username = username )
 
+@app.route('/add_car', methods=['POST', 'GET'])
+def add_car():
+    return render_template('add_car.html')
+
+@app.route('/car_added', methods=['POST', 'GET'])
+def car_added():
+    if request.method == 'POST':
+        make = request.form['make']
+        model =request.form['model']
+        year = request.form['year']
+        color = request.form['color']
+        price = request.form['price']
+        conn = sqlite3.connect('inventory.db')
+        cur = conn.cursor()
+        cur.execute('INSERT INTO inventory (make, model, year, color, price) VALUES (?,?,?,?,?)', (make, model, year, color, price))
+        conn.commit()
+        conn.close()
+    return render_template('car_added.html', make = make, model = model, year = year, color = color, price = price)
+
+@app.route('/sell_car', methods=['POST', 'GET'])
+def sell_car():
+    return render_template('sell_car.html')
+
+@app.route('/car_sold', methods=['POST', 'GET'])
+def car_sold():
+    if request.method == 'POST':
+        id = request.form['id']
+        # make = request.form['make']
+        # model =request.form['model']
+        # year = request.form['year']
+        # color = request.form['color']
+        # price = request.form['price']
+        conn = sqlite3.connect('inventory.db')
+        cur = conn.cursor()
+        cur.execute('DELETE FROM inventory WHERE id = {}'.format(id))
+        conn.commit()
+        conn.close()
+    return render_template('car_sold.html', id = id)
 
 if __name__ == '__main__':
     app.run(debug=True)
