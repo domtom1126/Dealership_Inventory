a25135e504c4df07e17504aa2e809a89657fec2e
domtom1126 <domtom1126@gmail.com>
2020-10-09 22:48:08 -0700

    Everything returning 200. Next to do is view lot page and profile page

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, flash, redirect, session
 import sqlite3
+from datetime import timedelta
+from flask.ext.bcrypt import Bcrypt
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
 conn = sqlite3.connect('user.db')
 
-
+app.secret_key='secret'
+app.permanent_session_lifetime = timedelta(minutes=5)
 
 @app.route('/', methods=['POST', 'GET'])
+def home():
+    return render_template('home.html')
+
+
+@app.route('/login', methods=['POST', 'GET'])
 def login():
-    return render_template('login.html')
+        return render_template('login.html')
 
 @app.route('/dashboard', methods=['POST','GET'])
 def dashboard():
@@ -24,6 +33,17 @@ def dashboard():
 
 @app.route('/add_car', methods=['POST', 'GET'])
 def add_car():
+    if request.method == 'POST':
+        make = request.form['make']
+        model =request.form['model']
+        year = request.form['year']
+        color = request.form['color']
+        price = request.form['price']
+        conn = sqlite3.connect('inventory.db')
+        cur = conn.cursor()
+        cur.execute('INSERT INTO inventory (make, model, year, color, price) VALUES (?,?,?,?,?)', (make, model, year, color, price))
+        conn.commit()
+        conn.close()
     return render_template('add_car.html')
 
 @app.route('/car_added', methods=['POST', 'GET'])
@@ -61,5 +81,12 @@ def car_sold():
         conn.close()
     return render_template('car_sold.html', id = id)
 
+@app.route('/view_lot', methods=['POST', 'GET'])
+def view_lot():
+    conn = sqlite3.connect('inventory.db')
+    cur = conn.cursor()
+    select_all = cur.execute('SELECT * FROM inventory')
+    display_all = select_all.fetchall()
+    return render_template('view_lot.html', display_all=display_all)
 if __name__ == '__main__':
     app.run(debug=True)
